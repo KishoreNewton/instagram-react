a37d2cce67ea8711802ed2b29e6c524e2d2f0546
KishoreNewton <kishorenke@gmail.com>
2020-09-18 04:29:03 +0530

    Added the not found page

---
+++ src/pages/not-found.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Layout from '../components/shared/Layout'
+import { Typography } from "@material-ui/core"
+import { Link } from 'react-router-dom'
+
+function NotFoundPage() {
+  return <Layout title="Page Not Found" marginTop={120}>
+    <Typography varient="h5" align="center" paragraph>
+      Sorry, this page isn't avaliable
+    </Typography>
+    <Typography align="center">
+      The link you followed may be broken, or the page may have been removed. 
+      <Link to="/">
+        <Typography color="primary" component="span">
+          {" "}Go back to Instagram
+        </Typography>
+      </Link>
+    </Typography>
+  </Layout>;
+}
+
+export default NotFoundPage;
