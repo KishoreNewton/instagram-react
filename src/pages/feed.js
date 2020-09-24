8efe69772361890d4ef791352376e906fe8e9513
KishoreNewton <kishorenke@gmail.com>
2020-09-24 17:58:01 +0530

    feat: added the bigged follow suggestion page

--- src/pages/feed.js
+++ src/pages/feed.js
@@ -19,8 +19,8 @@ function FeedPage() {
   return <Layout>
           <div className={classes.container}>
             <div>
-              {Array.from({ length: 5 }, () => getDefaultPost()).map(post => (
-                <FeedPost key={post.id} post={post} />
+              {Array.from({ length: 5 }, () => getDefaultPost()).map((post, index) => (
+                <FeedPost key={post.id} index={index} post={post} />
               ))}     
             </div>
             <Hidden smDown>
