96defd9d307f4b8a0328651ac08aead47da8555f
KishoreNewton <kishorenke@gmail.com>
2020-09-25 05:13:14 +0530

    feat: started explore grid

--- src/components/explore/ExploreGrid.js
+++ src/components/explore/ExploreGrid.js
@@ -1,10 +1,26 @@
-import React from "react";
-import { useExploreGridStyles } from "../../styles";
+import { Typography } from "@material-ui/core"
+import React from "react"
+import { useExploreGridStyles } from "../../styles"
+import { LoadingLargeIcon } from '../../icons'
+import { getDefaultPost } from '../../data'
 
 function ExploreGrid() {
-  useExploreGridStyles();
+  const classes = useExploreGridStyles()
 
-  return <div>ExploreGrid</div>;
+  let loading = true
+
+  return (
+    <>
+      <Typography color="textSecondary" variant="subtitle2" component="h2" gutterBottom className={classes.typography} >
+        Explore
+      </Typography>
+      {loading ? (
+        <LoadingLargeIcon />
+      ) : (
+
+      )}
+    </>
+  )
 }
 
-export default ExploreGrid;
+export default ExploreGrid
