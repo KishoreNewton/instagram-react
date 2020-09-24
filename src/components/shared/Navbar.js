aab194891c28d9a4d8e0084456892544b8789a19
KishoreNewton <kishorenke@gmail.com>
2020-09-24 09:30:30 +0530

    navbar fixed

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,16 +1,25 @@
-import { AppBar } from "@material-ui/core"
-import React from "react"
+import { AppBar, InputBase, Hidden } from "@material-ui/core"
+import React, { useState } from "react"
 import { useNavbarStyles } from "../../styles"
 import { Link } from 'react-router-dom'
 import logo from '../../images/logo.png'
+import { LoadingIcon } from '../../icons'
 
-function Navbar() {
+function Navbar({ minimalNavbar }) {
   const classes = useNavbarStyles()
 
   return (
     <AppBar className={classes.appBar}>
       <section className={classes.section}>
         <Logo />
+        {!minimalNavbar && <Search />}
+        {!minimalNavbar && <Links />}
+        {/* {!minimalNavabar && (
+          <>
+            <Search />
+            <Links />
+          </>
+        )} */}
       </section>
     </AppBar>
   )
@@ -27,6 +36,34 @@ function Logo() {
         </div>
       </Link>
     </div>
+  )
+}
+
+function Search() {
+  const classes = useNavbarStyles()
+  const [query, setQuery] = useState('')
+  function handleClearInput() {
+    setQuery('')
+  }
+  let loading = false
+  return (
+    <>
+      <Hidden xsDown>
+        <InputBase className={classes.input} onChange={event => setQuery(event.target.value)} startAdornment={<span className={classes.searchIcon}/>} endAdornment={ loading ? (
+          <LoadingIcon /> 
+        ) : (
+          <span onClick={handleClearInput} className={classes.clearIcon} />
+        )} placeholder="Search" value={query} />
+      </Hidden>
+    </>
+  )
+}
+
+function Links() {
+  return (
+    <>
+    
+    </>
   )
 }
 
