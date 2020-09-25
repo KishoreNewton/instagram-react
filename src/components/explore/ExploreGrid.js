b6024b50f2c80273a7e12f555b3faed602c992cd
KishoreNewton <kishorenke@gmail.com>
2020-09-25 05:31:15 +0530

    feat: added the site image

--- src/components/explore/ExploreGrid.js
+++ src/components/explore/ExploreGrid.js
@@ -3,11 +3,12 @@ import React from "react"
 import { useExploreGridStyles } from "../../styles"
 import { LoadingLargeIcon } from '../../icons'
 import { getDefaultPost } from '../../data'
+import GridPost from '../shared/GridPost'
 
 function ExploreGrid() {
   const classes = useExploreGridStyles()
 
-  let loading = true
+  let loading = false
 
   return (
     <>
@@ -17,7 +18,13 @@ function ExploreGrid() {
       {loading ? (
         <LoadingLargeIcon />
       ) : (
-
+        <article className={classes.article}>
+          <div className={classes.postContainer}>
+            {Array.from({ length: 20 }, () => getDefaultPost()).map(post => (
+              <GridPost key={post.id} post={post}  />
+            ))}
+          </div>
+        </article>
       )}
     </>
   )
