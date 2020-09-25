3630b22641c7c9576336738ed16079471923385c
KishoreNewton <kishorenke@gmail.com>
2020-09-25 06:52:15 +0530

    added the modal popup

--- src/components/shared/GridPost.js
+++ src/components/shared/GridPost.js
@@ -1,12 +1,22 @@
 import React from "react"
 import { useGridPostStyles } from "../../styles"
 import { Typography } from '@material-ui/core'
+import { useHistory } from "react-router-dom"
+
 
 function GridPost({ post }) { 
+  const history = useHistory()
   const classes = useGridPostStyles()
 
+  function handleOpenPostModal() {
+    history.push({
+      pathname: `/p/${post.id}`,
+      state: { modal: true }
+    })
+  }
+
   return (
-    <div className={classes.gridPostContainer}>
+    <div onClick={handleOpenPostModal} className={classes.gridPostContainer}>
       <div className={classes.gridPostOverlay}>
         <div className={classes.gridPostInfo}>
           <span className={classes.likes} />
@@ -20,8 +30,8 @@ function GridPost({ post }) {
             {post.comments.length}
           </Typography>
         </div>
-        <img src={post.media} alt="Post Cover" className={classes.image}  />
       </div>
+      <img src={post.media} alt="Post Cover" className={classes.image} />
     </div>
   )
 }
