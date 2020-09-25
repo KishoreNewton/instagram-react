690f82f122db1f678843f66a8ae4617602a87af2
KishoreNewton <kishorenke@gmail.com>
2020-09-25 11:57:17 +0530

    fixed the navbar failure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter as Router } from 'react-router-dom'
 import { MuiThemeProvider, CssBaseline } from "@material-ui/core"
 import theme from "./theme"
 import App from "./App"
-import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
