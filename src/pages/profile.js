79fa1d4d62f653c430964c10224f455535c5b5f6
KishoreNewton <kishorenke@gmail.com>
2020-09-28 01:37:14 +0530

    added mobile styling

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,13 +1,20 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/shared/Layout"
 import { useProfilePageStyles } from "../styles"
 import { defaultCurrentUser } from '../data'
-import { Card, CardContent, Hidden } from "@material-ui/core"
+import { Button, Card, CardContent, Hidden, Typography } from "@material-ui/core"
 import ProfilePicture from '../components/shared/ProfilePicture'
+import { Link } from "react-router-dom"
+import { GearIcon } from "../icons"
 
 function ProfilePage() {
   const isOwner = true
+  const [showOptionsMenu, setOptionsMenu] = useState(false)
   const classes = useProfilePageStyles()
+
+  function handleOptionsMenuClick() {
+    setOptionsMenu(true)
+  }
 
   return (
     <Layout title={`${defaultCurrentUser.name} (@${defaultCurrentUser.username})`} >
@@ -16,7 +23,7 @@ function ProfilePage() {
           <Card className={classes.cardLarge}>
             <ProfilePicture isOwner={isOwner} />
             <CardContent className={classes.cardContentLarge}>
-              <ProfileNameSection />
+              <ProfileNameSection user={defaultCurrentUser} isOwner={isOwner} handleOptionsMenuClick={handleOptionsMenuClick} />
               <PostCountSection />
               <NameBioSection />
             </CardContent>
@@ -27,7 +34,7 @@ function ProfilePage() {
             <CardContent>
               <section className={classes.sectionSmall}>
                 <ProfilePicture isOwner={isOwner} size={77} />
-                <ProfileNameSection />
+                <ProfileNameSection user={defaultCurrentUser} handleOptionsMenuClick={handleOptionsMenuClick} isOwner={isOwner}  />
               </section>
               <NameBioSection />
             </CardContent>
@@ -39,8 +46,74 @@ function ProfilePage() {
   )
 }
 
-function ProfileNameSection() {
-  return <>ProfileNameSection</>
+function ProfileNameSection({ user, isOwner, handleOptionsMenuClick }) {
+  const classes = useProfilePageStyles()
+  let followButton
+  const isFollowing = false
+  const isFollower = false
+  if (isFollowing) {
+    followButton = (
+      <Button variant="outlined" className={classes.button} >
+        Following
+      </Button>
+    ) 
+  } else if (isFollower) {
+    followButton = (
+      <Button variant="contained" color="primary" className={classes.button}>
+        Follow Back
+      </Button>
+    )
+  } else {
+    followButton = (
+      <Button variant="contained" color="primary" className={classes.button}>
+        Follow
+      </Button>
+    )
+  }
+  return (
+    <>
+      <Hidden xsDown>
+        <section className={classes.usernameSection}>
+          <Typography className={classes.username}>
+            {user.username}
+          </Typography>
+          {isOwner ? (
+            <>
+              <Link to="/acount/edit">
+                <Button variant="outlined">Edit Profile</Button>
+              </Link>
+              <div onClick={handleOptionsMenuClick} className={classes.settingsWrapper}>
+                <GearIcon className={classes.settings} />
+              </div>
+            </>
+          ) : (
+            <>
+              {followButton}
+            </>
+          )}
+        </section>
+      </Hidden>
+      <Hidden smUp>
+        <section>
+          <div className={classes.usernameDivSmall}>
+            <Typography className={classes.username}>
+              {user.username}
+            </Typography>
+            {isOwner && (
+              <div onClick={handleOptionsMenuClick} className={classes.settingsWrapper}>
+                <GearIcon className={classes.settings} />
+              </div>
+            )}
+          </div>
+          {isOwner ? (
+            <Link to="/acount/edit">
+              <Button variant="outlined" style={{ width: '100%' }}>Edit Profile</Button>
+            </Link>
+          ) : followButton}
+        </section>
+      </Hidden>
+    </>
+  )
 }
 
 function PostCountSection() {
