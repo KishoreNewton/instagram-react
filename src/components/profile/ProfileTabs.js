6347506547fc13209b2c3d83d87935a592827672
KishoreNewton <kishorenke@gmail.com>
2020-09-28 03:17:37 +0530

    feat: added the profile post sectiona and saved section

--- src/components/profile/ProfileTabs.js
+++ src/components/profile/ProfileTabs.js
@@ -1,7 +1,8 @@
-import { Divider, Hidden, Tab, Tabs } from "@material-ui/core"
+import { Divider, Hidden, Tab, Tabs, Typography } from "@material-ui/core"
 import React, { useState } from "react"
 import { GridIcon, SaveIcon } from "../../icons"
 import { useProfileTabsStyles } from "../../styles"
+import GridPost from '../shared/GridPost'
 
 function ProfileTabs({ user, isOwner }) {
   const classes = useProfileTabsStyles()
@@ -39,16 +40,55 @@ function ProfileTabs({ user, isOwner }) {
       <Hidden smUp>
         {user.posts.length === 0 && <Divider />}
       </Hidden>
+      {value === 0 && <ProfilePosts user={user} isOwner={isOwner} />}
+      {value === 1 && <SavedPosts />}
     </section>
   )
 }
 
-function ProfilePosts() {
+function ProfilePosts({ user, isOwner }) {
+  const classes = useProfileTabsStyles()
 
+  if(user.posts.length === 0) {
+    return(
+      <section className={classes.profilePostsSection}>
+        <div className={classes.noContent}>
+          <div className={classes.uploadPhotoIcon} />
+          <Typography variant="h4">
+            {isOwner ? "Upload a Photo" : "No Photos"}
+          </Typography>
+        </div>
+      </section>
+    )
+  }
+
+  return (
+    <article className={classes.article}>
+      <div className={classes.postContainer}>
+        {user.posts.map(post => (
+          <GridPost key={post.id} post={post} />
+        ))}
+      </div>
+    </article>
+  )
 }
 
 function SavedPosts() {
-  
+  const classes = useProfileTabsStyles()
+
+  return(
+    <section className={classes.savedPostsSection}>
+      <div className={classes.noContent}>
+        <div className={classes.savePhotoIcon} />
+        <Typography variant="h4">
+          Save
+        </Typography>
+        <Typography align="center">
+          Save photos and videos that you want to see again. No one is notified, and only you can see what you've saved.
+        </Typography>
+      </div>
+    </section>
+  )
 }
 
 export default ProfileTabs
