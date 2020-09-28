f713017c95ebd3c79ceaaedd593829aa2df8e9d9
KishoreNewton <kishorenke@gmail.com>
2020-09-28 10:53:42 +0530

    feat: started the edit-profile page

--- src/pages/edit-profile.js
+++ src/pages/edit-profile.js
@@ -1,10 +1,26 @@
-import React from "react"
+import React, { useState } from "react"
 import { useEditProfilePageStyles } from "../styles"
+import Layout from '../components/shared/Layout'
+import { IconButton } from "@material-ui/core"
+import { Menu } from '@material-ui/icons'
 
 function EditProfilePage() {
-  useEditProfilePageStyles()
+  const classes = useEditProfilePageStyles()
+  const [showDrawer, setDrawer] = useState(false)
 
-  return <div>EditProfilePage</div>
+  function handleToggleDrawer() {
+    setDrawer(prev => !prev)
+  }
+
+  return (
+    <Layout title="Edit Profile">
+      <section className={classes.section}>
+        <IconButton edge="start" onClick={handleToggleDrawer} className={classes.menuButton} >
+          <Menu />
+        </IconButton>
+      </section>
+    </Layout>
+  )
 }
 
 export default EditProfilePage
