d67e287939d16f63fc8f16d41ef056978ceb8431
KishoreNewton <kishorenke@gmail.com>
2020-10-04 06:07:20 +0530

    feat: post image feature completed

--- src/components/post/AddPostDialog.js
+++ src/components/post/AddPostDialog.js
@@ -5,8 +5,10 @@ import { Avatar, Paper, Toolbar, Typography, Dialog, Button, AppBar, TextField, 
 import { ArrowBackIos, PinDrop } from '@material-ui/icons'
 import { useAddPostDialogStyles } from '../../styles'
 import { UserContext } from '../../App'
-
-
+import serialize from '../../utils/serialize'
+import handleImageUpload from '../../utils/handleImageUpload'
+import { useMutation } from '@apollo/react-hooks';
+import { CREATE_POST } from '../../graphql/mutations'
 
 const initialValue = [{
     type: "paragraph",
@@ -17,8 +19,24 @@ function AddPostDialog({ media, handleClose }) {
     const classes = useAddPostDialogStyles()
     const editor = useMemo(() => withReact(createEditor()), [])
     const [value, setValue] = useState(initialValue)
-    const { me } = useContext(UserContext)
+    const { me, currentUserId } = useContext(UserContext)
     const [location, setLocation] = useState('')
+    const [isSubmitting, setSubmitting] = useState(false)
+    const [createPost] = useMutation(CREATE_POST)
+
+    async function handleSharePost() {
+        setSubmitting(true)
+        const url = await handleImageUpload(media)
+        const variables = {
+            userId: currentUserId,
+            location,
+            caption: serialize({ children: value }),
+            media: url
+        }
+        await createPost({ variables })
+        setSubmitting(false)
+        window.location.reload()
+    }
     
     return (
         <Dialog fullScreen open onClose={handleClose}>
@@ -28,7 +46,7 @@ function AddPostDialog({ media, handleClose }) {
                     <Typography align="center" variant="body1" className={classes.title} >
                         New Post
                     </Typography>
-                    <Button color="primary" className={classes.share}>
+                    <Button color="primary" className={classes.share} disabled={isSubmitting} onClick={handleSharePost}>
                         Share
                     </Button>
                 </Toolbar>
@@ -63,7 +81,7 @@ function AddPostDialog({ media, handleClose }) {
                     )
                     
                 }}
-                onChange={event => SecurityPolicyViolationEvent(event.target.value)}
+                onChange={event => setLocation(event.target.value)}
             />
         </Dialog>
     )
